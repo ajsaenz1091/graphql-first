775e2e077e0e6e1d1d5352b5516c191165985dcb
Alberto Saenz <albertosaenz@Albertos-MacBook-Pro.local>
2023-02-05 18:56:17 -0500

    changed folder structure to modularize schema types

--- server.js
+++ server.js
@@ -1,72 +1,21 @@
+const path = require('path')
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
 
+const { loadFilesSync } = require('@graphql-tools/load-files')
 const { makeExecutableSchema } = require('@graphql-tools/schema')
 
-const schemaText = `
-type Query {
-    products: [Product]
-    orders: [Order]
-}
-
-type Product {
-    id: ID!
-    description: String!
-    reviews: [Review]
-    price: Float!
-
-}
-
-type Review {
-    rating: Int!
-    comment: String
-}
-
-type Order {
-    date: String!
-    subtotal: Float!
-    items: [OrderItem]
-}
-
-type OrderItem {
-    product: Product!
-    quantity: Int!
-}
-`
+const typesArray = loadFilesSync('**/*', {
+    extensions: ['graphql'],
+  });
 
 const schema = makeExecutableSchema({
-    typeDefs: [schemaText]
+    typeDefs: typesArray
 })
 
 const root = {
-    products: [
-        {
-            id: 'redshoe',
-            description: 'Red Shoe',
-            price: 42.12,
-        },
-        {
-            id: 'bluejean',
-            description: 'Blue Jeans',
-            price: 55.55,
-        },
-    ],
-    orders: [
-        {
-            date: '2005-05-05',
-            subtotal: 90.22,
-            items: [
-                {
-                    product: {
-                        id: 'redshoe',
-                        description: 'Old Red Shoe',
-                        price: 45.11,
-                    },
-                    quantity: 2,
-                }
-            ]
-        }
-    ]
+    orders: require('./orders/orders.model'),
+    products: require('./products/products.model')
 }
 
 const app = express()
